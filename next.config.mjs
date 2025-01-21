12a15807bf7c0f00f7c2772651ef2e1144565206
Shahmeer Ali <problemssolve196@gmail.com>
2025-01-21 22:58:00 +0500

    ./

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    env: {
+      SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+      SANITY_DATASET: process.env.SANITY_DATASET,
+      SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+    },
+  };
+  
+  export default nextConfig;
+  
