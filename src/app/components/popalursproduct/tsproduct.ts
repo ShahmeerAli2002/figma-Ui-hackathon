60e6ea835786a8076efac30c1653d63c65e8c196
Shahmeer Ali <problemssolve196@gmail.com>
2025-01-21 11:59:46 +0500

    ./

--- src/app/components/popalursproduct/tsproduct.ts
+++ src/app/components/popalursproduct/tsproduct.ts
@@ -1,29 +1,29 @@
-import { ProductCardProps } from "./types";
+// import { ProductCardProps } from "./types";
 
-export const productsData: ProductCardProps[] = [
-  {
-    imageSrc: "/image/image5.png",
-    imageAlt: "The Poplar suede sofa in beige",
-    title: "The Poplar suede sofa",
-    price: 980,
-  },
-  {
-    imageSrc: "/image/images1.jpeg",
-    imageAlt: "The Dandy chair in brown leather",
-    title: "The Dandy chair",
-    price: 250,
-  },
+// export const productsData: ProductCardProps[] = [
+//   {
+//     imageSrc: "/image/image5.png",
+//     imageAlt: "The Poplar suede sofa in beige",
+//     title: "The Poplar suede sofa",
+//     price: 980,
+//   },
+//   {
+//     imageSrc: "/image/images1.jpeg",
+//     imageAlt: "The Dandy chair in brown leather",
+//     title: "The Dandy chair",
+//     price: 250,
+//   },
  
-  {
-    imageSrc: "/image/image6.png",
-    imageAlt: "The Silky Vase",
-    title: "The Silky Vase",
-    price: 125,
-  },
-  // {
-  //   imageSrc: "/images/lucy-lamp.jpg",
-  //   imageAlt: "The Lucy Lamp",
-  //   title: "The Lucy Lamp",
-  //   price: 399,
-  // },
-];
+//   {
+//     imageSrc: "/image/image6.png",
+//     imageAlt: "The Silky Vase",
+//     title: "The Silky Vase",
+//     price: 125,
+//   },
+//   // {
+//   //   imageSrc: "/images/lucy-lamp.jpg",
+//   //   imageAlt: "The Lucy Lamp",
+//   //   title: "The Lucy Lamp",
+//   //   price: 399,
+//   // },
+// ];
