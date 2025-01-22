fa5cd8f7e786981a102ba3f3e18bae370a9bd4cb
Shahmeer Ali <problemssolve196@gmail.com>
2025-01-22 20:35:38 +0500

    ./

--- src/sanity.ts
+++ src/sanity.ts
@@ -1,11 +1,10 @@
 import { createClient } from '@sanity/client';
-import dotenv from 'dotenv';
-dotenv.config();
+
 const client = createClient({
   projectId: "qb6wry8z",
   dataset: "production",
   apiVersion: '2025-01-17',
-  
+  token:"skzc2n38FAcWdKphbs2OIFuZqzDCTX3esplWSKdlJgFvvWehuqSaA8xdMOI3fArYTxmvhX1mClY4PRO72rYPh7Z0mWEc8LZDHnxHGAV9skPUrhqQDDC3zCUKth32bWFymZj1MbnKzekcuOoh8CAR227a4HshoeyiCeVsHhWswEhCBVPYkBNm",
   useCdn: true,
 });
 
